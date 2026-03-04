be1373a8ff16ec079091dedd50751a7aa742e884
j.j olveira 2009 <jorjinhocorrea@gmail.com>
2026-03-04 14:01:15 -0300

    feat: cria estrutura base do jogo com readline e variáveis de estado

---
+++ Jogo.js
@@ -0,0 +1,11 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let vida1 = 20
+let vida2 = 20
+let turno = 1
+
